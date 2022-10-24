15b2ce78b55f5bde8444fb37e82febce17f60620
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-24 14:08:21 -0300

    feat: add aulas meta tags

--- src/pages/aulas.js
+++ src/pages/aulas.js
@@ -1,4 +1,5 @@
 import Image from "next/future/image";
+import Head from "next/head";
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/navbar";
 import aulas from "../data/aulas.json";
@@ -7,14 +8,19 @@ export default function Aulas() {
     return (
     <section className="bg-white dark:bg-gray-900 min-h-screen">
         <Navbar></Navbar>
+        <Head>
+            <title>Aula particular com Clebson Augusto Fonseca</title>
+            <meta name="description" content="Aulas particulares com Clebson Augusto, aprenda javascript, react, node, express, next e todo ambiente Javascript. Primeira aula grátis." />
+        </Head>
         <main className="py-8">
             <div className="pt-8 px-4 mx-auto max-w-screen-xl text-center lg:pt-16 lg:px-12">
                 <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                     <span className="text-xs bg-indigo-500 rounded-full text-white px-4 py-1.5 mr-3">News</span> <span className="text-sm font-medium">Agora também temos bootcamps</span> 
                     <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                 </a>
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Aulas particulares de
-programação</h1>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    Aulas particulares de programação
+                </h1>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Aprenda a programar do zero ao avançado em aulas particulares 100% online!</p>
             </div>
             <div className="px-4 mx-auto max-w-screen-xl lg:px-6 ">
