a9c53a38593fb4a49b3e2f9333242a6b28d0a334
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-25 19:30:53 -0300

    feat: add google ads

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,6 +27,9 @@ function MyApp({ Component, pageProps }) {
       <meta property="twitter:image" content="/preview.png"></meta>
     </Head>
     <Component {...pageProps} />
+    <Script id='google ads' async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440029794243758"
+     crossorigin="anonymous">
+    </Script>
     <Script 
       id='ld+json'
       type='application/ld+json'
