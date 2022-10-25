4f131ff5c30cb42a77bc282ccbb1bb8340c898bd
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-25 11:50:50 -0300

    fix: improving SEO acessibility

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
     return (
         <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-            <div className="hidden justify-between items-center w-full lg:flex md:w-auto" id="navbar-sticky">
+            <div className="hidden justify-between items-center w-full lg:flex md:w-auto" id="navbar-sticky-left">
                 <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <Link href="/">
@@ -75,7 +75,7 @@ export default function Navbar() {
                 </ul>
             </div>
             <div className="flex lg:hidden">
-                <button  onClick={() => setToggleNav(!toggleNav)} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false" >
+                <button  onClick={() => setToggleNav(!toggleNav)} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky-left" aria-expanded="false" >
                     <span className="sr-only">Abrir menu principal</span>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
@@ -86,12 +86,15 @@ export default function Navbar() {
                 duration-500 z-50 ${toggleNav ? "left-0" : "left-[-100%] "}
                 `}
                 >
-                    <button
-                        type="button"
-                        onClick={() => setToggleNav(!toggleNav)}
-                    >
-                        <FaWindowClose size="32" />
-                    </button>
+                        <li>
+                            <button
+                            type="button"
+                            aria-label="fechar menu"
+                            onClick={() => setToggleNav(!toggleNav)}
+                        >
+                            <FaWindowClose size="32" />
+                            </button>
+                        </li>
                         <li>
                             <button
                                 type="button"
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ export default function Home() {
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h1 className="mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white">Clebson Augusto Fonseca</h1>
 
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-              <li class="inline-flex items-center">
-                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                  <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+              <li className="inline-flex items-center">
+                <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                  <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
                   Home
                 </a>
               </li>
