4fe0b61bd21dadff7e41b3da7bf43c7bcf050f45
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-11-02 16:15:54 -0300

    hotfix: fixing styling padding and add conquista

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import Image from 'next/future/image'
-import Head from 'next/head'
-import Link from 'next/link'
-import Footer from '../components/footer/footer'
-import Navbar from '../components/navbar/navbar'
+import Image from "next/future/image"
+import Head from "next/head"
+import Link from "next/link"
+import Footer from "../components/footer/footer"
+import Navbar from "../components/navbar/navbar"
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
       <main className="mt-32 mx-4">
         <div className="py-16 px-8 mx-auto max-w-screen-xl text-left lg:grid lg:grid-cols-2 bg-regal-purple dark:bg-gray-800 border rounded-lg ">
-          <div className='flex-col items-center pb-12 md:pb-0'>
+          <div className="pb-12 md:pb-0 items-center justify-center">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-3xl dark:text-white">
               Chegou a hora de dominar sua nova stack de uma forma descomplicada.
             </h1>
@@ -56,12 +56,11 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          <div className='flex justify-center pt-0 sm:pt-8'>
-            <img src='/developer.png' width={450} />
+          <div className="flex justify-center pt-0 sm:pt-8">
+            <Image src="/developer.png" width={450} height={300} alt="Ilustração de um desenvolvedor"/>
           </div>
         </div>
-        <div className="gap-16 items-center  px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 ">
-      
+        <div className="gap-16 items-center px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 py-8 lg:py-16 lg:px-6 ">
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 ">
               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">🔭 Quem sou eu?</h2>
               <h1 className="text-2xl tracking-tight font-extrabold text-black">Me chamo <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Clebson Augusto Fonseca</span></h1>
@@ -69,19 +68,18 @@ export default function Home() {
                 Programador como profissão e professor por paixão. Atualmente trabalho na @Ledthinking e na @LedthinkingAcademy, startups que fundei. Programo desde os meus 12 anos e desde daquela época busco sempre evoluir cada vez mais, para celebrar esses 10 anos de programação, resolvi criar esse site para compartilhar um pouco da minha tragetoria e também servir como um diretório de tudo que produzo.
               </p>
           </div>
-          <div className="grid grid-cols-2 gap-4 mt-8">
-              <Image width={348.5} height={618} className="w-full rounded-lg" priority src="/clebson-2019.jpeg" alt="Clebson Augusto Fonseca no ano de 2019" />
+          <div className="grid grid-cols-2 gap-4 mt-8 filter saturate-150">
+              <Image width={348.5} height={618} className="w-full rounded-lg " priority src="/clebson-2019.jpeg" alt="Clebson Augusto Fonseca no ano de 2019" />
               <Image width={348.5} height={618} className="mt-4 w-full lg:mt-10 rounded-lg" priority={false} src="/clebson-2022.jpg" alt="Clebson Augusto fonseca no ano de 2022"/>
           </div>
-          <img src='/superprof.png' alt="Banner clebson site superprof." />
+          <Image width={400} height={400} src="/superprof.png" alt="Banner clebson site superprof." />
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="text-4xl tracking-tight font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-orange-400">Sou um superprof</h2>
+              <h2 className="text-4xl tracking-tight font-extrabold "><span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-orange-400">Sou um superprof</span> 🦸🏻‍♂️</h2>
               <p className="mb-4"> 
                 Em 2021 fui classificado como um superprofessor na plataforma superprof. Desde então tem sido um prazer ministrar aulas para todos os diversos niveis e os mais variados assuntos de programação.
               </p>
           </div>
         </div>
-
         <section className="bg-white dark:bg-gray-900">
           <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
               <h2 className="pb-4 text-4xl tracking-tight font-extrabold">
@@ -111,12 +109,19 @@ export default function Home() {
           <ol className="relative border-l border-gray-200 dark:border-gray-700">                  
               <li className="mb-10 ml-4">
                   <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                  <span className="leading-none text-gray-500 dark:text-gray-600">Concedido(s) por Campus party ·<time dateTime="2020-08-06 17:00" className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">ago 2021</time></span>
+                  <span className="leading-none text-gray-500 dark:text-gray-600">Concedido(s) por Auth0 ·<time dateTime="2022-10-31 17:00" className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">octo 2022</time></span>
+                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    Auth0 Ambassador
+                  </h3>
+              </li>
+              <li className="mb-10 ml-4">
+                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                  <span className="leading-none text-gray-500 dark:text-gray-600">Concedido(s) por Campus party ·<time dateTime="2021-08-06 17:00" className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">ago 2021</time></span>
                   <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                     1º lugar Hackathon Reboot The World, Campus party Digital - Goiás - 2021
                   </h3>
                   <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Durante o ano 2021, finalmente veio o tão sonhado primeiro lugar. Tinhamos lançado nossa ferramenta de dados abertos, strike.tours, uma rede social que formentaria o turismo da região de Goiás.</p>
-                  <a target="_blank" className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" href='https://www.desenvolvimento.go.gov.br/noticias/4177-passaporte-digital-para-turistas-vence-hackathon-na-campus-party-digital-goias.html' rel="noreferrer">Leia sobre</a>
+                  <a target="_blank" className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" href="https://www.desenvolvimento.go.gov.br/noticias/4177-passaporte-digital-para-turistas-vence-hackathon-na-campus-party-digital-goias.html" rel="noreferrer">Leia sobre</a>
               </li>
               <li className="mb-10 ml-4">
                   <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -144,7 +149,7 @@ export default function Home() {
             <div className="mx-auto max-w-screen-md sm:text-center pb-50">
                 <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">Cadastre-se no newsletter</h2>
                 <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Mantenha-se atualizado com anúncios e descontos exclusivos sinta-se à vontade para se inscrever com seu e-mail.</p>
-                <form className='pb-8' action="#">
+                <form className="pb-8" action="#">
                     <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                         <div className="relative w-full">
                             <label htmlFor="email" className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
